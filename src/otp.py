cb08099b70d417d53fd9e130a8c355f369ee8402
IcQuackson <pedrorenato1998@gmail.com>
2024-12-09 17:57:32 +0000

    Changed hotp to totp as project subject is ambiguous

--- src/otp.py
+++ src/otp.py
@@ -1,13 +1,19 @@
 import hashlib
 import hmac
 import struct
+import time
 
-def hotp(key, counter):
+TIME_STEP = 30
+EPOCH = 0
+
+def totp(key):
 	key_bin = bytes.fromhex(key)
-	counter_bin = struct.pack(">Q", counter) # '>Q' means big-endian unsigned long long (8 bytes)
+	current_time = int(time.time())
+	time_counter = (current_time - EPOCH) // TIME_STEP
+	time_counter_bin = struct.pack(">Q", time_counter) # '>Q' means big-endian unsigned long long (8 bytes)
 
 	# Create hmac-sha1 object
-	hmac_sha1 = hmac.new(key_bin, counter_bin, hashlib.sha1)
+	hmac_sha1 = hmac.new(key_bin, time_counter_bin, hashlib.sha1)
 
 	# Result in bytes
 	hmac_result =  hmac_sha1.digest()
@@ -35,17 +41,16 @@ def hotp(key, counter):
 	code = extended_code % 10**6
 
 	# Zero-pad to ensure it has exactly 6 digits
-	otp_str = f'{code:06d}'  # Format as a zero-padded 6-digit string
+	totp_str = f'{code:06d}'  # Format as a zero-padded 6-digit string
 
-	print(otp_str)
-
-
+	print(totp_str)
 
 
 
-counter = 0
+
+
 key = "75ee7604145f8ee3ebbacd719a7d836fb44edb6c17dc6572cc5c455e0b12f433"
 
-hotp(key, counter)
+totp(key)
 #print(otp)
 
